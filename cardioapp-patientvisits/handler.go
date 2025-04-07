f2034d5cec17b15ee39726b0a7b73eb421e026df
ibrokhimov.fr <fr.ibrokhimov@gmail.com>
2025-04-07 15:07:24 +0500

    Parse incoming request and handle errors

--- cardioapp-patientvisits/handler.go
+++ cardioapp-patientvisits/handler.go
@@ -23,3 +23,20 @@ import (
 const (
 	appId = "P-JV2nVIRUtgyPO5xRNeYll2mT4F5QG4bS"
 )
+
+func Handle(req []byte) string {
+	var response Response
+	var request Request
+	const urlConst = "https://api.admin.u-code.io"
+
+	err := json.Unmarshal(req, &request)
+	if err != nil {
+		return Handler("error 1", err.Error())
+	}
+
+	requestData := request.Data["object_data"].(map[string]interface{})
+	naznacheniyaData, err, _ := GetSingleObject(urlConst, "naznachenie", cast.ToString(requestData["guid"]))
+	if err != nil {
+		return Handler("error 3", err.Error())
+	}
+}
